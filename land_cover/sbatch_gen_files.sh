3b1a4f0ac3554335703cb0b07c31c22682ad0c12
Alan Tong <kaitai@alumni.ubc.ca>
2022-05-23 03:36:33 -0700

    Update sbatch for land cover deploy

--- land_cover/sbatch_gen_files.sh
+++ land_cover/sbatch_gen_files.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=land_cover_deploy
 #SBATCH --account=def-ramankut-ab
+#SBATCH --gpus-per-node=p100:2
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=32G
-#SBATCH --time=06:00:00
+#SBATCH --time=0:15:00
 
 module load StdEnv/2020
 module load python/3.6
