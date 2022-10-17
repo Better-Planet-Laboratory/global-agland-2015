848174291c5d148bff9f5321f2c4b035b5e3d71c
Alan Tong <kaitai@alumni.ubc.ca>
2022-10-16 18:58:55 -0700

    Add subnational total by regions

--- evaluation/FAO_total_comp_by_continent.py
+++ evaluation/FAO_total_comp_by_continent.py
@@ -8,10 +8,226 @@ from census_processor import World
 from utils.tools.geo import crop_intermediate_state
 from utils.agland_map import *
 from utils.process.post_process import make_nonagricultural_mask
+from census_processor import Argentina, Australia, Austria, Belgium, Brazil, \
+    Bulgaria, Canada, China, Croatia, Cyprus, Czechia, \
+    Denmark, Estonia, Ethiopia, Finland, France, Germany, \
+    Greece, Hungary, India, Indonesia, Ireland, Italy, Kazakhstan, \
+    Latvia, Lithuania, Luxembourg, Malta, Mexico, Mongolia, \
+    Mozambique, Namibia, Netherlands, Pakistan, Poland, Portugal, \
+    Romania, Russia, SaudiArabia, Slovakia, Slovenia, SouthAfrica, \
+    Spain, Sweden, Tanzania, Turkey, Uganda, UK, Ukraine, USA, World
 
-WORLD_CENSUS = World('../shapefile/World/gadm36_0.shp',
-                     '../FAOSTAT_data/FAOSTAT_data_11-14-2020.csv',
-                     '../FAOSTAT_data/FAOcountryProfileUTF8_withregions.csv')
+CENSUS_SETTING_CFG = io.load_yaml_config('configs/census_setting_cfg.yaml')
+SHAPEFILE_CFG = io.load_yaml_config('configs/shapefile_cfg.yaml')
+SUBNATIONAL_STATS_CFG = io.load_yaml_config(
+    'configs/subnational_stats_cfg.yaml')
+
+SUBNATIONAL_CENSUS = {
+    'Argentina':
+    Argentina(SHAPEFILE_CFG['path_dir']['Argentina'],
+              SUBNATIONAL_STATS_CFG['path_dir']['Argentina'],
+              CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Australia':
+    Australia(SHAPEFILE_CFG['path_dir']['Australia'],
+              SUBNATIONAL_STATS_CFG['path_dir']['Australia'],
+              CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Austria':
+    Austria(SHAPEFILE_CFG['path_dir']['Austria'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Austria'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Belgium':
+    Belgium(SHAPEFILE_CFG['path_dir']['Belgium'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Belgium'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Brazil':
+    Brazil(SHAPEFILE_CFG['path_dir']['Brazil'],
+           SUBNATIONAL_STATS_CFG['path_dir']['Brazil'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Bulgaria':
+    Bulgaria(SHAPEFILE_CFG['path_dir']['Bulgaria'],
+             SUBNATIONAL_STATS_CFG['path_dir']['Bulgaria'],
+             CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Canada':
+    Canada(SHAPEFILE_CFG['path_dir']['Canada'],
+           SUBNATIONAL_STATS_CFG['path_dir']['Canada'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'China':
+    China(SHAPEFILE_CFG['path_dir']['China'],
+          SUBNATIONAL_STATS_CFG['path_dir']['China'],
+          CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Croatia':
+    Croatia(SHAPEFILE_CFG['path_dir']['Croatia'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Croatia'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Cyprus':
+    Cyprus(SHAPEFILE_CFG['path_dir']['Cyprus'],
+           SUBNATIONAL_STATS_CFG['path_dir']['Cyprus'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Czechia':
+    Czechia(SHAPEFILE_CFG['path_dir']['Czechia'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Czechia'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Denmark':
+    Denmark(SHAPEFILE_CFG['path_dir']['Denmark'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Denmark'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Estonia':
+    Estonia(SHAPEFILE_CFG['path_dir']['Estonia'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Estonia'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Ethiopia':
+    Ethiopia(SHAPEFILE_CFG['path_dir']['Ethiopia'],
+             SUBNATIONAL_STATS_CFG['path_dir']['Ethiopia'],
+             CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Finland':
+    Finland(SHAPEFILE_CFG['path_dir']['Finland'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Finland'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'France':
+    France(SHAPEFILE_CFG['path_dir']['France'],
+           SUBNATIONAL_STATS_CFG['path_dir']['France'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Germany':
+    Germany(SHAPEFILE_CFG['path_dir']['Germany'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Germany'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Greece':
+    Greece(SHAPEFILE_CFG['path_dir']['Greece'],
+           SUBNATIONAL_STATS_CFG['path_dir']['Greece'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Hungary':
+    Hungary(SHAPEFILE_CFG['path_dir']['Hungary'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Hungary'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'India':
+    India(SHAPEFILE_CFG['path_dir']['India'],
+          SUBNATIONAL_STATS_CFG['path_dir']['India'],
+          CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Indonesia':
+    Indonesia(SHAPEFILE_CFG['path_dir']['Indonesia'],
+              SUBNATIONAL_STATS_CFG['path_dir']['Indonesia'],
+              CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Ireland':
+    Ireland(SHAPEFILE_CFG['path_dir']['Ireland'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Ireland'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Italy':
+    Italy(SHAPEFILE_CFG['path_dir']['Italy'],
+          SUBNATIONAL_STATS_CFG['path_dir']['Italy'],
+          CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Kazakhstan':
+    Kazakhstan(SHAPEFILE_CFG['path_dir']['Kazakhstan'],
+               SUBNATIONAL_STATS_CFG['path_dir']['Kazakhstan'],
+               CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Latvia':
+    Latvia(SHAPEFILE_CFG['path_dir']['Latvia'],
+           SUBNATIONAL_STATS_CFG['path_dir']['Latvia'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Lithuania':
+    Lithuania(SHAPEFILE_CFG['path_dir']['Lithuania'],
+              SUBNATIONAL_STATS_CFG['path_dir']['Lithuania'],
+              CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Luxembourg':
+    Luxembourg(SHAPEFILE_CFG['path_dir']['Luxembourg'],
+               SUBNATIONAL_STATS_CFG['path_dir']['Luxembourg'],
+               CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Malta':
+    Malta(SHAPEFILE_CFG['path_dir']['Malta'],
+          SUBNATIONAL_STATS_CFG['path_dir']['Malta'],
+          CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Mexico':
+    Mexico(SHAPEFILE_CFG['path_dir']['Mexico'],
+           SUBNATIONAL_STATS_CFG['path_dir']['Mexico'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Mongolia':
+    Mongolia(SHAPEFILE_CFG['path_dir']['Mongolia'],
+             SUBNATIONAL_STATS_CFG['path_dir']['Mongolia'],
+             CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Mozambique':
+    Mozambique(SHAPEFILE_CFG['path_dir']['Mozambique'],
+               SUBNATIONAL_STATS_CFG['path_dir']['Mozambique'],
+               CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Namibia':
+    Namibia(SHAPEFILE_CFG['path_dir']['Namibia'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Namibia'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Netherlands':
+    Netherlands(SHAPEFILE_CFG['path_dir']['Netherlands'],
+                SUBNATIONAL_STATS_CFG['path_dir']['Netherlands'],
+                CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Pakistan':
+    Pakistan(SHAPEFILE_CFG['path_dir']['Pakistan'],
+             SUBNATIONAL_STATS_CFG['path_dir']['Pakistan'],
+             CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Poland':
+    Poland(SHAPEFILE_CFG['path_dir']['Poland'],
+           SUBNATIONAL_STATS_CFG['path_dir']['Poland'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Portugal':
+    Portugal(SHAPEFILE_CFG['path_dir']['Portugal'],
+             SUBNATIONAL_STATS_CFG['path_dir']['Portugal'],
+             CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Romania':
+    Romania(SHAPEFILE_CFG['path_dir']['Romania'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Romania'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Russia':
+    Russia(SHAPEFILE_CFG['path_dir']['Russia'],
+           SUBNATIONAL_STATS_CFG['path_dir']['Russia'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'SaudiArabia':
+    SaudiArabia(SHAPEFILE_CFG['path_dir']['SaudiArabia'],
+                SUBNATIONAL_STATS_CFG['path_dir']['SaudiArabia'],
+                CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Slovakia':
+    Slovakia(SHAPEFILE_CFG['path_dir']['Slovakia'],
+             SUBNATIONAL_STATS_CFG['path_dir']['Slovakia'],
+             CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Slovenia':
+    Slovenia(SHAPEFILE_CFG['path_dir']['Slovenia'],
+             SUBNATIONAL_STATS_CFG['path_dir']['Slovenia'],
+             CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'SouthAfrica':
+    SouthAfrica(SHAPEFILE_CFG['path_dir']['SouthAfrica'],
+                SUBNATIONAL_STATS_CFG['path_dir']['SouthAfrica'],
+                CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Spain':
+    Spain(SHAPEFILE_CFG['path_dir']['Spain'],
+          SUBNATIONAL_STATS_CFG['path_dir']['Spain'],
+          CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Sweden':
+    Sweden(SHAPEFILE_CFG['path_dir']['Sweden'],
+           SUBNATIONAL_STATS_CFG['path_dir']['Sweden'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Tanzania':
+    Tanzania(SHAPEFILE_CFG['path_dir']['Tanzania'],
+             SUBNATIONAL_STATS_CFG['path_dir']['Tanzania'],
+             CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Turkey':
+    Turkey(SHAPEFILE_CFG['path_dir']['Turkey'],
+           SUBNATIONAL_STATS_CFG['path_dir']['Turkey'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Uganda':
+    Uganda(SHAPEFILE_CFG['path_dir']['Uganda'],
+           SUBNATIONAL_STATS_CFG['path_dir']['Uganda'],
+           CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'UK':
+    UK(SHAPEFILE_CFG['path_dir']['UK'],
+       SUBNATIONAL_STATS_CFG['path_dir']['UK'],
+       CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'Ukraine':
+    Ukraine(SHAPEFILE_CFG['path_dir']['Ukraine'],
+            SUBNATIONAL_STATS_CFG['path_dir']['Ukraine'],
+            CENSUS_SETTING_CFG['path_dir']['FAOSTAT']),
+    'USA':
+    USA(SHAPEFILE_CFG['path_dir']['USA'],
+        SUBNATIONAL_STATS_CFG['path_dir']['USA'],
+        CENSUS_SETTING_CFG['path_dir']['FAOSTAT'])
+}
+
+WORLD_CENSUS = World('shapefile/World/gadm36_0.shp',
+                     'FAOSTAT_data/FAOSTAT_data_11-14-2020.csv',
+                     'FAOSTAT_data/FAOcountryProfileUTF8_withregions.csv')
 
 
 def pack_continent_counts_in_table(pred_continent_count, FAO_continent_count):
@@ -53,25 +269,33 @@ def main():
         "--agland_map_dir",
         type=str,
         default=
-        '../outputs/all_correct_to_FAO_scale_itr3_fr_0/agland_map_output_3.tif',
+        'outputs/all_correct_to_FAO_scale_itr3_fr_0/agland_map_output_3.tif',
         help="path dir to agland_map for evaluation")
     parser.add_argument("--water_body_dir",
                         type=str,
-                        default='../land_cover/water_body_mask.tif',
+                        default='land_cover/water_body_mask.tif',
                         help="path dir to water body mask tif")
     parser.add_argument("--gdd_filter_map_dir",
                         type=str,
-                        default='../gdd/gdd_filter_map_360x720.tif',
+                        default='gdd/gdd_filter_map_360x720.tif',
                         help="path dir to gdd filter map tif")
     parser.add_argument("--global_area_map_dir",
                         type=str,
-                        default='../land_cover/global_area_2160x4320.tif',
+                        default='land_cover/global_area_2160x4320.tif',
                         help="path dir to global area map tif")
     args = parser.parse_args()
     print(args)
 
+    # ==================== [SELECT FAO / SUBNATIONAL TOTAL] ====================
     # Load world census table from FAOSTAT
-    global_census_table = WORLD_CENSUS.merge_all()
+    # global_census_table = WORLD_CENSUS.census_table
+
+    # Load world census table from subnational
+    # CENSUS_SETTING_CFG['calibrate'] should be all False, otherwise will be the same as FAOSTAT
+    global_census_table = WORLD_CENSUS.replace_subnation(
+        SUBNATIONAL_CENSUS, CENSUS_SETTING_CFG['calibrate'])
+    # ==========================================================================
+
     num_states = len(global_census_table)
 
     # Load agland map and apply masks
@@ -79,8 +303,11 @@ def main():
     nonagricultural_mask = make_nonagricultural_mask(
         shape=(agland_map.height, agland_map.width),
         mask_dir_list=[args.water_body_dir, args.gdd_filter_map_dir])
-    # Our mask also include GDD - try not to apply mask
+
+    # ======= [SELECT WITH / WITHTOUT GDD+WATERBODY MASK ON AGLAND MAP] =======
     # agland_map.apply_mask(nonagricultural_mask)
+    # ==========================================================================
+
     cropland_map = agland_map.get_cropland().copy()
     pasture_map = agland_map.get_pasture().copy()
 
@@ -125,10 +352,11 @@ def main():
             ])
 
         FAO_continent_count[global_census_table.iloc[i]
-                            ['REGIONS']] += np.asarray([
-                                global_census_table.iloc[i]['CROPLAND'],
-                                global_census_table.iloc[i]['PASTURE']
-                            ])
+                            ['REGIONS']] += np.nan_to_num(
+                                np.asarray([
+                                    global_census_table.iloc[i]['CROPLAND'],
+                                    global_census_table.iloc[i]['PASTURE']
+                                ]), 0)
 
     comparison_table = pack_continent_counts_in_table(pred_continent_count,
                                                       FAO_continent_count)
--- utils/process/census_process.py
+++ utils/process/census_process.py
@@ -2,7 +2,8 @@ from ..tools.census_core import *
 from gdd.gdd_criteria import gdd_crop_criteria
 
 
-def pipeline(world_census, subnational_census, census_setting_cfg, gdd_cfg, land_cover_cfg):
+def pipeline(world_census, subnational_census, census_setting_cfg, gdd_cfg,
+             land_cover_cfg):
     """
     Census processing pipeline:
     1. Merge WORLD with SUBNATIONAL
@@ -27,42 +28,54 @@ def pipeline(world_census, subnational_census, census_setting_cfg, gdd_cfg, land
     census_states_count_table_dir = census_setting_cfg['path_dir']['outputs']['calibration_factors_table'], \
                                     census_setting_cfg['path_dir']['outputs']['census_states_count_table']
     if calibration_factors_table_dir is not None:
-        write_calibration_factors_table_to_csv(get_calibration_factors_table(subnational_census), 
-                                               calibration_factors_table_dir)
+        write_calibration_factors_table_to_csv(
+            get_calibration_factors_table(subnational_census),
+            calibration_factors_table_dir)
     if census_states_count_table_dir is not None:
-        write_census_states_count_table(count_census_states(subnational_census),
-                                        census_states_count_table_dir)
+        write_census_states_count_table(
+            count_census_states(subnational_census),
+            census_states_count_table_dir)
 
     # Merge WORLD with SUBNATIONAL
-    merged_census = merge_subnation_to_world(world_census, subnational_census, census_setting_cfg['calibrate'])
-    print('Merge WORLD census with SUBNATIONAL census. Total samples: {}'.format(len(merged_census)))
+    merged_census = merge_subnation_to_world(world_census, subnational_census,
+                                             census_setting_cfg['calibrate'])
+    print(
+        'Merge WORLD census with SUBNATIONAL census. Total samples: {}'.format(
+            len(merged_census)))
 
     # Apply 2 filters
     # 1. nan_filter: nan in either CROPLAND or PASTURE
     # 2. GDD_filter: GDD exclude: above 50d north with < 1000
     #                GDD include / (GDD exclude + GDD include) < accept_ratio
     merged_census = apply_nan_filter(merged_census)
-    merged_census = apply_GDD_filter(merged_census, gdd_cfg, census_setting_cfg['GDD_filter']['accept_ratio'],
-                                     gdd_crop_criteria)
-    print('Apply nan_filter and GDD_filter. Total samples: {}'.format(len(merged_census)))
+    merged_census = apply_GDD_filter(
+        merged_census, gdd_cfg,
+        census_setting_cfg['GDD_filter']['accept_ratio'], gdd_crop_criteria)
+    print('Apply nan_filter and GDD_filter. Total samples: {}'.format(
+        len(merged_census)))
 
     # Add land_cover percentage features to census table
-    merged_census = add_land_cover_percentage(merged_census, land_cover_cfg['path_dir']['land_cover_map'],
-                                              land_cover_cfg['code']['MCD12Q1'],
-                                              land_cover_cfg['null_value'])
-    print('Add land_cover percentage to census. Total samples: {}'.format(len(merged_census)))
+    merged_census = add_land_cover_percentage(
+        merged_census, land_cover_cfg['path_dir']['land_cover_map'],
+        land_cover_cfg['code']['MCD12Q1'], land_cover_cfg['null_value'])
+    print('Add land_cover percentage to census. Total samples: {}'.format(
+        len(merged_census)))
 
     # Add state area in kHa for each sample
-    merged_census = add_state_area(merged_census, land_cover_cfg['path_dir']['global_area_map'],
-                                   land_cover_cfg['area_unit'])
-    print('Add state area to census. Total samples: {}'.format(len(merged_census)))
+    merged_census = add_state_area(
+        merged_census, land_cover_cfg['path_dir']['global_area_map'],
+        land_cover_cfg['area_unit'])
+    print('Add state area to census. Total samples: {}'.format(
+        len(merged_census)))
 
     # Add agland percentage for each sample
     merged_census = add_agland_percentage(merged_census)
-    print('Add cropland/pasture/other percentage to census. Total samples: {}'.format(len(merged_census)))
+    print('Add cropland/pasture/other percentage to census. Total samples: {}'.
+          format(len(merged_census)))
 
     # Save final census table as pkl
-    processed_census_table_dir = census_setting_cfg['path_dir']['outputs']['processed_census_table']
+    processed_census_table_dir = census_setting_cfg['path_dir']['outputs'][
+        'processed_census_table']
     if processed_census_table_dir is not None:
         write_census_table_to_pkl(merged_census, processed_census_table_dir)
 
