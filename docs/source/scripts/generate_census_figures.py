c87035c35db1b67e72c491c3b9d3c44682bcbf8a
Alan Tong <kaitai@alumni.ubc.ca>
2023-01-08 22:27:51 -0800

    Update generate_census_figures.py

--- docs/source/scripts/generate_census_figures.py
+++ docs/source/scripts/generate_census_figures.py
@@ -139,7 +139,7 @@ def main():
 
     # Merge FAO with subnational census
     merged_census = merge_subnation_to_world(WORLD_CENSUS, SUBNATIONAL_CENSUS,
-                                             CENSUS_SETTING_CFG['calibrate'])
+                                             CENSUS_SETTING_CFG)
     print('Total Initial Number of samples: {}'.format(len(merged_census)))
 
     # Mark nan filter
